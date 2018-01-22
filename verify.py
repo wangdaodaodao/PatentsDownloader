19a4fae22a6eb9c96adfd6af200fe5b93fba5fdf
wangdaodao7 <ahking0516@126.com>
2018-01-22 23:05:55 +0800

    update

--- verify.py
+++ verify.py
@@ -7,25 +7,11 @@ import requests
 
 verify_url = 'http://www2.drugfuture.com/cnpat/verifyCode.aspx'
 
-def verify(pantentno='CN200910083739.2'):
-
-    data = {
-        'cnpatentno': pantentno,
-        'Common': '1'
-    }
-
-    response = requests.post(verify_url, headers=headers_verify, data = data)
-
-    print(response)
-
-
 def verifyCode():
     response = requests.get(verify_url, headers=headers_verifycode)
     with open('yzm.jpg', 'wb') as code:
         code.write(response.content)
     yzm = input('输入验证：>>')
-
     return yzm
-
 
 print(verifyCode())
