580c8203ff43789ac4cea600c5a9237f7c53c149
wangdaodao7 <ahking0516@126.com>
2018-01-23 00:04:37 +0800

    验证码识别

--- verify.py
+++ verify.py
@@ -3,15 +3,25 @@ from config import *
 from bs4 import BeautifulSoup
 import lxml
 import requests
-
+from PIL import Image
+import pytesseract
 
 verify_url = 'http://www2.drugfuture.com/cnpat/verifyCode.aspx'
+
+
+
+
+
 
 def verifyCode():
     response = requests.get(verify_url, headers=headers_verifycode)
     with open('yzm.jpg', 'wb') as code:
         code.write(response.content)
     yzm = input('输入验证：>>')
+
+    img = Image.open('yzm.jpg')
+    vcode = pytesseract.image_to_string(img)
+    print(vcode)
     return yzm
 
 print(verifyCode())
