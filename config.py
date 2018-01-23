e3a0787e2657823d3c1c857f540543f0390b67b9
wangdaodao7 <ahking0516@126.com>
2018-01-23 15:09:06 +0800

    update

--- config.py
+++ config.py
@@ -4,23 +4,27 @@ MAX_PAGE_NUMS = 5
 
 
 headers_verify = {
-    'Referer': 'http://www.drugfuture.com/cnpat/cn_patent.asp',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Origin': 'http://www.drugfuture.com',
-    'Host': 'www2.drugfuture.com',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Connection': 'keep-alive',
-    'Accept-Language': 'en-us',
-    'Accept-Encoding': 'gzip, deflate',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7',
-    'Content-Length': '36',
-    'Upgrade-Insecure-Requests': '1',
+    'Accept-Encoding':'gzip, deflate',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language':'zh-CN,zh;q=0.8',
+    'Cache-Control':'no-cache',
+    'Connection':'keep-alive',
+    'Content-Length':'36',
+    'Content-Type':'application/x-www-form-urlencoded',
+    'Host':'www2.drugfuture.com',
+    'Origin':'http://www.drugfuture.com',
+    'Pragma':'no-cache',
+    'Referer':'http://www.drugfuture.com/cnpat/cn_patent.asp',
+    'Upgrade-Insecure-Requests':'1',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.4399.400 QQBrowser/9.7.12777.400',
+
+
 }
 
 
 
 
-header_search = {
+headers_search = {
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding':'gzip, deflate',
     'Accept-Language':'zh-CN,zh;q=0.8',
--- search.py
+++ search.py
@@ -4,16 +4,19 @@ from config import *
 from verify import *
 
 
-
+code = verifyCode('CN201510708735.4')
 def search_patent(name='CN201510708735.4'):
     url = 'http://www2.drugfuture.com/cnpat/search.aspx'
+    
+
     data = {
         'cnpatentno':name,
         'common':'1',
-        'ValidCode':verifyCode(),
+        'ValidCode':code,
     }
 
-    response = requests.get(url, data=data, headers=headers_verifycode)
+    response = requests.post(url, data=data, headers=headers_search)
+    print(code)
     print(response.text)
 
 
