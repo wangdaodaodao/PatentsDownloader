55966941fada01e6d16b80874534d58d327f4049
wangdaodao7 <ahking0516@126.com>
2018-01-23 15:38:26 +0800

    解决了验证码问题

--- search.py
+++ search.py
@@ -1,23 +1,48 @@
 import requests
 from patentid import *
 from config import *
-from verify import *
+import re
 
 
-code = verifyCode('CN201510708735.4')
-def search_patent(name='CN201510708735.4'):
-    url = 'http://www2.drugfuture.com/cnpat/search.aspx'
-    
+session = requests.Session()
+no = 'CN201510708735.4'
 
+verify_url = 'http://www2.drugfuture.com/cnpat/verify.aspx'
+verifycode_url = 'http://www2.drugfuture.com/cnpat/verifyCode.aspx'
+search_url = 'http://www2.drugfuture.com/cnpat/search.aspx'
+
+
+def verifyCode():
     data = {
-        'cnpatentno':name,
+    'cnpatentno':no,
+    'Common':'1'
+    }
+    response_1 = session.post(verify_url, headers=headers_verify, data =data )
+
+
+    response_2 = session.get(verifycode_url)
+    with open('yzm.jpg', 'wb') as code:
+        code.write(response_2.content)
+    yzm = input('输入验证：>>')
+
+    return yzm
+
+
+
+
+
+
+def search_patent():
+ 
+    data = {
+        'cnpatentno':no,
         'common':'1',
-        'ValidCode':code,
+        'ValidCode':verifyCode(),
     }
 
-    response = requests.post(url, data=data, headers=headers_search)
-    print(code)
+    response = session.post(search_url, data=data, headers=headers_search)
     print(response.text)
 
+    pattern = re.compile('(.*?)')
 
 search_patent()
