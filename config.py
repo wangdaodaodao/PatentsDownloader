9ed8bab0651acf7b7e0576381c74d4e668b5b7e0
wangdaodao7 <ahking0516@126.com>
2018-01-23 23:39:55 +0800

    update

--- config.py
+++ config.py
@@ -53,10 +53,10 @@ headers_verifycode = {
         
 }
 
-headers_save_patent = {
+headers_securepdf = {
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding':'gzip, deflate',
-    'Accept-Language':'zh-CN,zh;q=0.8',
+    'Accept-Language':'en-us',
     'Cache-Control':'no-cache',
     'Connection':'keep-alive',
     'Content-Length':'415',
@@ -81,4 +81,7 @@ headers_getpdf = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.4399.400 QQBrowser/9.7.12777.400'
         
     }
+
+    
+
 
--- patendown.py
+++ patendown.py
@@ -18,10 +18,10 @@ def get_pdf(no = 'CN201510708735.4'):
         'cnpatentno': no,
         'Common': '1'
     }
-    response_verify_1 = session.post(verify_url, headers=headers_verify, data=data_verify)
-    response_verify_2 = session.get(verifycode_url)
+    response_verify = session.post(verify_url, headers=headers_verify, data=data_verify)
+    response_verifycode = session.get(verifycode_url)
     with open('yzm.jpg', 'wb') as code:
-        code.write(response_verify_2.content)
+        code.write(response_verifycode.content)
     yzm = input('输入验证：>>')
 
     data_search = {
@@ -31,34 +31,34 @@ def get_pdf(no = 'CN201510708735.4'):
     }
     response_search = session.post(search_url, data=data_search, headers=headers_search)
     pattern = re.compile('<input name="PatentNo" value=(.*?) type="hidden" /><input name="Name" value="(.*?)" type="hidden" /><input name="PatentType" value="(.*?)" type="hidden" /><input name="PageNumFM" value="(.*?)" type="hidden" /><input name="UrlFM" value="(.*?)" type="hidden" /><input name="PageNumSD" value="(.*?)" type="hidden" /><input name="UrlSD" value="(.*?)"type="hidden"  /><input name="PublicationDate" value="(.*?)" type="hidden" /><input name="ReadyType" value="(.*?)" type="hidden" /><input name="FulltextType" value="(.*?)" type="hidden" /><input name="Common" value="(.*?)" type="hidden" /></form>')
-    tt =list( pattern.findall(response_search.text)[0])
+    search_data =list( pattern.findall(response_search.text)[0])
     host_pattern = re.compile('{document.Download.action="(.*?)"')
-    tt2 = host_pattern.findall(response_search.text)[3].split('//')[1].split('/')[0]
+    host_name = host_pattern.findall(response_search.text)[3].split('//')[1].split('/')[0]
     
     print()
-    data_savepdf = {
-        'PatentNo':tt[0],
-        'Name':tt[1],
-        'PatentType':tt[2],
-        'PageNumFM':tt[3],
-        'UrlFM':tt[4],
-        'PageNumSD':tt[5],
-        'UrlSD':tt[6],
-        'PublicationDate':tt[7],
-        'ReadyType':tt[8],
-        'FulltextType':tt[9],
-        'Common':tt[10],
+    data_securepdf = {
+        'PatentNo':search_data[0],
+        'Name':search_data[1],
+        'PatentType':search_data[2],
+        'PageNumFM':search_data[3],
+        'UrlFM':search_data[4],
+        'PageNumSD':search_data[5],
+        'UrlSD':search_data[6],
+        'PublicationDate':search_data[7],
+        'ReadyType':search_data[8],
+        'FulltextType':search_data[9],
+        'Common':search_data[10],
     }
 
-    headers_save_patent['Host'] = tt2
-    save_patent_url = 'http://{}/cnpat/SecurePdf.aspx'.format(tt2)
-    print(save_patent_url)
-    response_savepdf = session.post(save_patent_url, headers=headers_save_patent, data = data_savepdf)
+    headers_securepdf['Host'] = host_name
+    securepdf_url = 'http://{}/cnpat/SecurePdf.aspx'.format(host_name)
+    print(securepdf_url, data_securepdf)
+    response_securepdf = session.post(securepdf_url, headers=headers_securepdf, data = data_securepdf)
 
-    headers_getpdf['Host'] = tt2
-    headers_getpdf['Referer'] = 'http://{}/cnpat/SecurePdf.aspx'.format(tt2)
+    headers_getpdf['Host'] = host_name
+    headers_getpdf['Referer'] = 'http://{}/cnpat/SecurePdf.aspx'.format(host_name)
 
-    file_url = 'http://{}/cnpat/package/%E5%8F%91%E6%98%8E%E4%B8%93%E5%88%A9%E7%94%B3%E8%AF%B7%E8%AF%B4%E6%98%8E%E4%B9%A6{}.pdf'.format(tt2, no)
+    file_url = 'http://{}/cnpat/package/%E5%8F%91%E6%98%8E%E4%B8%93%E5%88%A9%E7%94%B3%E8%AF%B7%E8%AF%B4%E6%98%8E%E4%B9%A6{}.pdf'.format(host_name, no)
      
 
 
@@ -70,3 +70,5 @@ def get_pdf(no = 'CN201510708735.4'):
 
 get_pdf()
 
+# http://www10.drugfuture.com/cnpat/package/发明专利申请说明书CN201510708735.4.pdf
+# http://www11.drugfuture.com/cnpat/package/发明专利申请说明书CN201510708735.4.pdf
