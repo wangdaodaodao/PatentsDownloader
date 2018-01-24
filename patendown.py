6f3f6359412af116500519d7978ee6e74b220f3e
wangdaodao7 <ahking0516@126.com>
2018-01-24 10:28:13 +0800

    update

--- patendown.py
+++ patendown.py
@@ -24,13 +24,14 @@ def get_pdf(no='CN201510708735.4'):
     response_verifycode = session.get(verifycode_url)
     with open('yzm.jpg', 'wb') as code:
         code.write(response_verifycode.content)
-    yzm = input('输入验证：>>')
+    yzm = input('输入验证码：>>')
 
     data_search = {
         'cnpatentno': no,
         'common': '1',
         'ValidCode': yzm,
     }
+    print('正在获取专利信息：')
     response_search = session.post(
         search_url, data=data_search, headers=headers_search)
     pattern = re.compile('<input name="PatentNo" value="(.*?)" type="hidden" /><input name="Name" value="(.*?)" type="hidden" /><input name="PatentType" value="(.*?)" type="hidden" /><input name="PageNumFM" value="(.*?)" type="hidden" /><input name="UrlFM" value="(.*?)" type="hidden" /><input name="PageNumSD" value="(.*?)" type="hidden" /><input name="UrlSD" value="(.*?)"type="hidden"  /><input name="PublicationDate" value="(.*?)" type="hidden" /><input name="ReadyType" value="(.*?)" type="hidden" /><input name="FulltextType" value="(.*?)" type="hidden" /><input name="Common" value="(.*?)" type="hidden" /></form>')
@@ -38,10 +39,9 @@ def get_pdf(no='CN201510708735.4'):
     host_pattern = re.compile('{document.Download.action="(.*?)"')
     host_name = host_pattern.findall(response_search.text)[
         3].split('//')[1].split('/')[0]
-
     FulltextType_pattern = re.compile('document.Download.FulltextType.value="(.*?)"')
     FulltextType_value = FulltextType_pattern.findall(response_search.text)[3]
-    print(FulltextType_value)
+    
     data_securepdf = {
         'PatentNo': search_data[0],
         'Name': search_data[1],
@@ -57,15 +57,10 @@ def get_pdf(no='CN201510708735.4'):
     }
 
     headers_securepdf['Host'] = host_name
-
-
     response_securepdf = session.post(
         securepdf_url.format(host_name=host_name), headers=headers_securepdf, data=data_securepdf)
-
-
     file_url = 'http://{}/cnpat/package/%E5%8F%91%E6%98%8E%E4%B8%93%E5%88%A9%E7%94%B3%E8%AF%B7%E8%AF%B4%E6%98%8E%E4%B9%A6{}.pdf'.format(
         host_name, no)
-
 
     with open('{}.pdf'.format(no), 'wb') as code:
         code.write(session.get(file_url).content)
