16460eea98360275b1d753aff8d70619572e4349
wangdaodao7 <ahking0516@126.com>
2018-02-01 22:22:37 +0800

    下载进度

--- down.py
+++ down.py
@@ -1,53 +1,60 @@
 import requests
 
-class ProgressBar(object):
+import sys, time
 
-    def __init__(self, title,
-                 count=0.0,
-                 run_status=None,
-                 fin_status=None,
-                 total=100.0,
-                 unit='', sep='/',
-                 chunk_size=1.0):
-        super(ProgressBar, self).__init__()
-        self.info = "【%s】%s %.2f %s %s %.2f %s"
-        self.title = title
-        self.total = total
-        self.count = count
-        self.chunk_size = chunk_size
-        self.status = run_status or ""
-        self.fin_status = fin_status or " " * len(self.status)
-        self.unit = unit
-        self.seq = sep
+class ShowProcess():
+    """
+    显示处理进度的类
+    调用该类相关函数即可实现处理进度的显示
+    """
+    i = 0 # 当前的处理进度
+    max_steps = 0 # 总共需要处理的次数
+    max_arrow = 50 #进度条的长度
 
-    def __get_info(self):
-        # 【名称】状态 进度 单位 分割线 总数 单位
-        _info = self.info % (self.title, self.status,
-                             self.count/self.chunk_size, self.unit, self.seq, self.total/self.chunk_size, self.unit)
-        return _info
+    # 初始化函数，需要知道总共的处理次数
+    def __init__(self, max_steps):
+        self.max_steps = max_steps
+        self.i = 0
+        self.show_process()
 
-    def refresh(self, count=1, status=None):
-        self.count += count
-        # if status is not None:
-        self.status = status or self.status
-        end_str = "\r"
-        if self.count >= self.total:
-            end_str = '\n'
-            self.status = status or self.fin_status
-        print(self.__get_info(), end=end_str)
+    # 显示函数，根据当前的处理进度i显示进度
+    # 效果为[>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]100.00%
+    def show_process(self, i=None):
+        if i is not None:
+            self.i = i
+        else:
+            self.i += 1
+        num_arrow = int(self.i * self.max_arrow / self.max_steps) #计算显示多少个'>'
+        num_line = self.max_arrow - num_arrow #计算显示多少个'-'
+        percent = self.i * 100.0 / self.max_steps #计算完成进度，格式为xx.xx%
+        process_bar = '[' + '>' * num_arrow + '-' * num_line + ']'\
+                    + '%.2f' % percent + '%' + '\r' #带输出的字符串，'\r'表示不换行回到最左边
+        sys.stdout.write(process_bar) #这两句打印字符到终端
+        sys.stdout.flush()
 
+    def close(self, words='done'):
+        print ()
+        print (words)
+        self.i = 0
 
-
-with closing(requests.get('https://ss2.bdstatic.com/lfoZeXSm1A5BphGlnYG/skin_plus/877.jpg?2', stream=True)) as response:
-    chunk_size = 1024 # 单次请求最大值
-    content_size = int(response.headers['content-length']) # 内容体总大小
-    progress = ProgressBar('1.jpg', total=content_size,
-                                     unit="KB", chunk_size=chunk_size, run_status="正在下载", fin_status="下载完成")
+def down_file(url='https://ss2.bdstatic.com/lfoZeXSm1A5BphGlnYG/skin_plus/877.jpg?2'):
+    response = requests.get(url, stream=True)
+    max_steps =   round(int(response.headers['content-length'])/1000) # 内容体总大小
+    print( max_steps)
+    process_bar = ShowProcess(max_steps)
     with open('1.jpg', "wb") as file:
-       for data in response.iter_content(chunk_size=chunk_size):
-           file.write(data)
-           progress.refresh(count=len(data))
+        for data in response.iter_content(chunk_size=1024):
+            
+            file.write(data)
+            process_bar.show_process()
+            
+        process_bar.close()
+            
 
+
+down_file()
+
+ 
 
 
 
