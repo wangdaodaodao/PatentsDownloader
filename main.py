c0c4142f2daa529fe1338355d0f69124afcf0297
wangdaodao7 <ahking0516@126.com>
2018-02-02 20:09:20 +0800

    修正错误

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from patentid import *
 
 print('请选择：\n1：输入专利号下载\n2：输入关键词批量下载')
 isDown = True
+check = True
 
 choice = input('输入1or2：')
 if choice == str(1):
@@ -16,17 +17,19 @@ elif choice == str(2):
         while isDown:
             for i in get_id(keywords, page_num):
                 get_pdf(i)
-            selection = input('第{}页现在完毕，按1或者2选择是否下载下一页：\n1为是，2为否：'.format(page_num))
-            if selection == str(1):
-                page_num += 1                
-            elif selection == str(2):
-                isDown = False
-                print('取消下载！')
-            else:
-                print('输入错误！')
-                isDown = False
+            while check:
+                selection = input('第{}页现在完毕，按1或者2选择是否下载下一页：\n1为是，2为否：'.format(page_num))
+                if selection == str(1):
+                    page_num += 1
+                    check = False                
+                elif selection == str(2):
+                    isDown = False
+                    print('取消下载！')
+                    check = False
+                else:
+                    print('输入错误,重新输入！')
     else:
-        print('关键词为空')            
+        print('关键词为空。')            
 else:
-    print('输入错误请返回')
+    print('输入错误请返回。')
 
